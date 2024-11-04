6ab35dc5dd6acac34ce72ca9ae8e5b27f09a53d3
Kel Modderman <kelvmod@gmail.com>
2024-11-04 20:30:48 +1000

    Check that /boot/efi is also on USB bus device.

--- helpers/calamares-bootloader-config-root-on-usb.py
+++ helpers/calamares-bootloader-config-root-on-usb.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3 -tt
 # -*- coding: utf-8 -*-
 
-__author__    = 'Kel Modderman'
-__copyright__ = '(C) 2024 Kel Modderman <kelvmod@gmail.com>'
-__license__   = 'GPLv2 or any later version'
+__author__      = 'Kel Modderman'
+__copyright__   = '(C) 2024 Kel Modderman <kelvmod@gmail.com>'
+__license__     = 'GPLv2 or any later version'
+__description__ = 'Return 0 if / and /boot/efi on a USB bus device'
 
 import pyudev
 import sys
 
-def get_root_device():
+def get_device_mounted_on(mount_pnt):
     with open('/proc/mounts', 'r') as mounts:
         entries = mounts.readlines()
         for entry in entries:
             device, mount = entry.split()[:2]
-            if (mount == '/'):
+            if (mount == mount_pnt):
                 return device
 
     return None
 
-def run(root_device_node = None):
-    if root_device_node is None:
+def run(child_device_node=None):
+    if child_device_node is None:
         sys.exit(1)
 
     context = pyudev.Context()
-    root_device = pyudev.Devices.from_device_file(context, root_device_node)
+    root_device = pyudev.Devices.from_device_file(context, child_device_node)
     try:
         parent_device_node = root_device.find_parent('block').device_node
     except AttributeError as e:
@@ -41,5 +42,11 @@ if __name__ == '__main__':
     try:
         root = sys.argv[1]
     except IndexError:
-        root = get_root_device()
+        root = get_device_mounted_on('/')
     run(root)
+
+    try:
+        boot = sys.argv[1]
+    except IndexError:
+        boot = get_device_mounted_on('/boot/efi')
+    run(boot)
